c70a96ec827adf3490d520660ecc8148d7e4a7ea
Margot Liard <margliard@gmail.com>
2021-11-10 17:00:21 +0100

    rails3 yelp done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,7 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.new(review_params)
+    @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
@@ -24,7 +24,7 @@ class RestaurantsController < ApplicationController
 
   private
 
-  def review_params
+  def restaurant_params
     params.require(:restaurant).permit(:name, :address, :category)
   end
 end
